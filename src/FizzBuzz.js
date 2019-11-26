60b1e7d72ab975ead97c2a15e9abac99d76c876e
raulcodurance <raul.mordillo@codurance.com>
2019-11-26 10:15:58 +0100

    extract methods

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,15 +1,24 @@
 export default class FizzBuzz {
     play(number) {
-        if (number % 5 == 0 && number % 3 == 0) {
+        if (isDivisibleBy5(number) && isDivisibleBy3(number)) {
             return "FizzBuzz";
         }
-        if (number % 5 == 0) {
+        if (isDivisibleBy5(number)) {
             return "Buzz";
         }
 
-        if (number % 3 == 0) {
+        if (isDivisibleBy3(number)) {
             return "Fizz";
         }
         return number.toString();
     }
 }
+
+function isDivisibleBy3(number) {
+    return number % 3 == 0;
+}
+
+function isDivisibleBy5(number) {
+    return number % 5 == 0;
+}
+
